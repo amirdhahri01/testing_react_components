2ebb5a843937a53024454e39088fa6e123d8eef5
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-15 18:09:52 +0100

    An update

--- src/LoginComponent.test.tsx
+++ src/LoginComponent.test.tsx
@@ -18,11 +18,13 @@ describe("Login Component Tests" , () => {
     beforeEach(() => {
         setup();
     })
+
     it("Should render correctly the login component" , () => {
         const mainElement = screen.getByRole("main")
         expect(mainElement).toBeInTheDocument()
         expect(screen.queryByTestId("resultLabel")).not.toBeInTheDocument()
     })
+
     it("Should render correctly - query by test id" , ()=>{
         const inputs = screen.getAllByTestId("input")
         expect(inputs).toHaveLength(3)
@@ -38,10 +40,17 @@ describe("Login Component Tests" , () => {
         expect(inputs[1].getAttribute("value")).toBe("")
         expect(inputs[2].getAttribute("value")).toBe("Login")
     })
+    
     it("Click login button with incomplete credentials - show required message" , () => {
         const inputs = screen.getAllByTestId('input')
         const loginButton = inputs[2];
         fireEvent.click(loginButton)
+        const resultLabel = screen.getByTestId("resultLabel")
+        expect(resultLabel.textContent).toBe("UserName and password required!")
+    })
+    it("Click login button with incomplete credentials - show required message" , () => {
+        const inputs = screen.getAllByTestId('input')
+        const loginButton = inputs[2];
         user.click(loginButton)
         const resultLabel = screen.getByTestId("resultLabel")
         expect(resultLabel.textContent).toBe("UserName and password required!")
