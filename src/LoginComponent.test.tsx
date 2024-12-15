f654d99c483ffc1fb7b7c07176248f5f6eec54ab
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-15 17:35:17 +0100

    An update

--- src/LoginComponent.test.tsx
+++ src/LoginComponent.test.tsx
@@ -1,7 +1,18 @@
+import { render } from "@testing-library/react"
+import LoginComponent from "./LoginComponent"
 
 
 describe("Login Component Tests" , () => {
+    const loginServiceMock ={
+        login : jest.fn()
+    }
+    const setTokenMock = jest.fn()
     it("Should render correctly the login component" , () => {
-
+        const container = render(<LoginComponent 
+            loginService={loginServiceMock} 
+            setToken={setTokenMock}
+            />).container
+        console.log(container.textContent);
+            
     })
 })
